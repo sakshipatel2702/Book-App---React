efce74371418381129af0c507c074406fc4b382e
Sakshi Patel <sakshipatel2702@gmail.com>
2023-02-10 14:46:13 -0500

    integrated node and react for registration of users

--- src/components/RegisterUsers.tsx
+++ src/components/RegisterUsers.tsx
@@ -1,27 +1,56 @@
 import React, { useState } from 'react'; 
 
 const RegisterUsers = (props: any) => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
+    // const [name, setName] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+    const [username, setName] = useState('');
     const [password, setPassword] = useState('');
+    const [contact, setContact] = useState('');
+    const [birthdate, setBirthdate] = useState('');
 
     const onSubmitClicked = (e: { preventDefault: () => void; }) => {
         e.preventDefault();
-        console.log(name);
+        console.log(username, password, contact, birthdate);
+        fetch("http://localhost:5000/register",{
+            method: "POST",
+            //crossDomain:true,
+            headers:{
+                "Content-Type":"application/json",
+                Accept:"application/json",
+                "Access-Control-Allow-Origin":"*",
+            },
+            body:JSON.stringify({
+                username,
+                password,
+                contact,
+                birthdate,
+            }),
+
+        }).then((res) => res.json())
+        .then((data) => {
+            console.log(data, "userRegister");
+        });
      }
 
     return (
     <>
     <div className="auth-form-container">
       <form className="register-form" onSubmit={onSubmitClicked}>
-         <label htmlFor="name">Name:</label>
-         <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter your Name here.." name="name" id="name"/>
+         <label htmlFor="username">User Name:</label>
+         <input value={username} onChange={(e) => setName(e.target.value)} placeholder="Enter your Name here.." name="username" id="username"/>
 
-         <label htmlFor="email">E-mail:</label>
-         <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" name="email" id="email"/>
+         {/* <label htmlFor="email">E-mail:</label>
+         <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" name="email" id="email"/> */}
 
          <label htmlFor="password">Password:</label>
          <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" name="password" id="password"/>
+
+         <label htmlFor="contact">Contact:</label>
+         <input value={contact} onChange={(e) => setContact(e.target.value)} type="contact" placeholder="Enter your Phone Number.." name="contact" id="contact"/>
+
+         <label htmlFor="birthdate">E-mail:</label>
+         <input value={birthdate} onChange={(e) => setBirthdate(e.target.value)} type="birthdate" placeholder="Enter your Birthday in MM/DD/YYYY" name="birthdate" id="birthdate"/>
 
          <button type="submit">Register</button>
       </form>
